54d4d8129f7506049e8bb06f911632975b2e6e40
Craig Tanis <craig-tanis@utc.edu>
2017-08-28 15:39:43 -0400

    initial

---
+++ empty_gameloop/src/GameLoop.java
@@ -0,0 +1,104 @@
+import org.lwjgl.Sys;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.LWJGLException;
+
+
+public class GameLoop
+{
+    public static final int TARGET_FPS=100;
+    public static final int SCR_WIDTH=800;
+    public static final int SCR_HEIGHT=600;
+
+    public static void main(String[] args) throws LWJGLException
+    {
+        initGL(SCR_WIDTH, SCR_HEIGHT);
+
+
+        int x=10;
+        int y=10;
+        int w=10;
+
+
+        while (! Display.isCloseRequested())
+        {
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+
+            // look for key presses
+
+            if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT))
+            {
+                x++;
+            }
+
+            if (Keyboard.isKeyDown(Keyboard.KEY_LEFT))
+            {
+                x--;
+            }
+
+            if (Keyboard.isKeyDown(Keyboard.KEY_DOWN))
+            {
+                y++;
+            }
+
+            if (Keyboard.isKeyDown(Keyboard.KEY_UP))
+            {
+                y--;
+            }
+
+
+
+            // draw the square
+            
+            GL11.glColor3f(0,1,0);
+            GL11.glBegin(GL11.GL_QUADS);
+
+            GL11.glVertex2f(x, y);
+            GL11.glVertex2f(x+w, y);
+            GL11.glVertex2f(x+w, y+w);
+            GL11.glVertex2f(x, y+w);
+
+            GL11.glEnd();
+
+
+            // UPDATE DISPLAY
+            Display.update();
+            Display.sync(TARGET_FPS);
+        }
+
+        Display.destroy();
+    }
+    
+
+    public static void initGL(int width, int height) throws LWJGLException
+    {
+        // open window of appropriate size
+        Display.setDisplayMode(new DisplayMode(width, height));
+        Display.create();
+        Display.setVSyncEnabled(true);
+        
+        // enable 2D textures
+        GL11.glEnable(GL11.GL_TEXTURE_2D);              
+     
+        // set "clear" color to black
+        GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);         
+
+        // enable alpha blending
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+         
+        // set viewport to entire window
+        GL11.glViewport(0,0,width,height);
+         
+        // set up orthographic projectionr
+        GL11.glMatrixMode(GL11.GL_PROJECTION);
+        GL11.glLoadIdentity();
+        GL11.glOrtho(0, width, height, 0, 1, -1);
+        // GLU.gluPerspective(90f, 1.333f, 2f, -2f);
+        // GL11.glTranslated(0, 0, -500);
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+    }
+}
---
+++ lib/README.txt
@@ -0,0 +1,1 @@
+put lwjgl and slick-util here
